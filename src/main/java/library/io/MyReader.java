234e5704dfeae7e811375300ce264c0ee6d6112c
yuuki1n <tus.yuuki@gmail.com>
2024-02-23 03:03:42 +0900

    テスト環境系

--- src/main/java/library/io/MyReader.java
+++ src/main/java/library/io/MyReader.java
@@ -2,12 +2,11 @@ package library.io;
 
 import static java.lang.Math.*;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
-import library.util.Util;
+import library.util.*;
 
-public class MyReader{
+public class MyReader extends Util{
   private byte[] buf = new byte[1 <<16];
   private int ptr,tail;
   private InputStream in;
@@ -34,15 +33,15 @@ public class MyReader{
 
   public int it(){ return toIntExact(lg()); }
 
-  public int[] it(int N){ return Util.arrI(N,i -> it()); }
+  public int[] it(int N){ return arrI(N,i -> it()); }
 
-  public int[][] it(int H,int W){ return Util.arr(new int[H][],i -> it(W)); }
+  public int[][] it(int H,int W){ return arr(new int[H][],i -> it(W)); }
 
   public int idx(){ return it() -1; }
 
-  public int[] idx(int N){ return Util.arrI(N,i -> idx()); }
+  public int[] idx(int N){ return arrI(N,i -> idx()); }
 
-  public int[][] idx(int H,int W){ return Util.arr(new int[H][],i -> idx(W)); }
+  public int[][] idx(int H,int W){ return arr(new int[H][],i -> idx(W)); }
 
   public long lg(){
     byte i = nextPrintable();
@@ -53,19 +52,19 @@ public class MyReader{
     return negative ? -n : n;
   }
 
-  public long[] lg(int N){ return Util.arrL(N,i -> lg()); }
+  public long[] lg(int N){ return arrL(N,i -> lg()); }
 
-  public long[][] lg(int H,int W){ return Util.arr(new long[H][],i -> lg(W)); }
+  public long[][] lg(int H,int W){ return arr(new long[H][],i -> lg(W)); }
 
   public double dbl(){ return Double.parseDouble(str()); }
 
-  public double[] dbl(int N){ return Util.arrD(N,i -> dbl()); }
+  public double[] dbl(int N){ return arrD(N,i -> dbl()); }
 
-  public double[][] dbl(int H,int W){ return Util.arr(new double[H][],i -> dbl(W)); }
+  public double[][] dbl(int H,int W){ return arr(new double[H][],i -> dbl(W)); }
 
   public char[] ch(){ return str().toCharArray(); }
 
-  public char[][] ch(int H){ return Util.arr(new char[H][],i -> ch()); }
+  public char[][] ch(int H){ return arr(new char[H][],i -> ch()); }
 
   public String line(){
     StringBuilder sb = new StringBuilder();
@@ -82,5 +81,5 @@ public class MyReader{
     return sb.toString();
   }
 
-  public String[] str(int N){ return Util.arr(new String[N],i -> str()); }
+  public String[] str(int N){ return arr(new String[N],i -> str()); }
 }
