9b5b6754d2cb253bea623e03e8b491aeb894024e
yuuki1n <tus.yuuki@gmail.com>
2024-07-15 18:58:49 +0900

    Util

--- src/main/java/library/util/Util.java
+++ src/main/java/library/util/Util.java
@@ -9,7 +9,7 @@ import java.util.function.*;
 public class Util{
   public static String yes = "Yes",no = "No";
   public static int infI = (1 <<30) -1;
-  public static long infL = (1L <<60 |1 <<30) -1,
+  public static long infL = (1L <<61 |1 <<30) -1,
       mod = 998244353;
   public static Random rd = ThreadLocalRandom.current();
   private long st = System.currentTimeMillis();
@@ -18,25 +18,25 @@ public class Util{
 
   protected void reset(){ st = System.currentTimeMillis(); }
 
-  public int[] arrI(int N,IntUnaryOperator f){
+  public static int[] arrI(int N,IntUnaryOperator f){
     int[] ret = new int[N];
     setAll(ret,f);
     return ret;
   }
 
-  public long[] arrL(int N,IntToLongFunction f){
+  public static long[] arrL(int N,IntToLongFunction f){
     long[] ret = new long[N];
     setAll(ret,f);
     return ret;
   }
 
-  public double[] arrD(int N,IntToDoubleFunction f){
+  public static double[] arrD(int N,IntToDoubleFunction f){
     double[] ret = new double[N];
     setAll(ret,f);
     return ret;
   }
 
-  public <T> T[] arr(T[] arr,IntFunction<T> f){
+  public static <T> T[] arr(T[] arr,IntFunction<T> f){
     setAll(arr,f);
     return arr;
   }
