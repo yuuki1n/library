081d343b1b814853aeae43f36d635e117bd2fe4a
tusyu <tusyu@192.168.1.19>
2025-03-01 20:30:35 +0900

    DynamicUnionFind

--- src/main/java/library/graph/unionfind/DynamicUnionFind.java
+++ src/main/java/library/graph/unionfind/DynamicUnionFind.java
@@ -1,7 +1,9 @@
 package library.graph.unionfind;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.*;
+import java.util.*;
+
+import library.util.*;
 
 /**
  * 動的UnionFind
@@ -9,13 +11,14 @@ import java.util.Map;
  *
  * @param <K>
  */
-public abstract class DynamicUnionFind<K> {
+public class DynamicUnionFind<K> {
   int num;
-  private Map<K, K> par;
+  private Map<K, K> par,nxt;
   private Map<K, Integer> size;
 
   public DynamicUnionFind(){
     par = new HashMap<>();
+    nxt = new HashMap<>();
     size = new HashMap<>();
     num = 0;
   }
@@ -24,6 +27,7 @@ public abstract class DynamicUnionFind<K> {
     if (par.containsKey(x))
       return false;
     par.put(x,x);
+    nxt.put(x,x);
     size.put(x,1);
     num++;
     return true;
@@ -40,17 +44,35 @@ public abstract class DynamicUnionFind<K> {
     return r;
   }
 
-  public boolean same(K u,K v){ return root(u).equals(root(v)); }
+  boolean same(K u,K v){ return root(u).equals(root(v)); }
 
-  public boolean unite(K u,K v){
+  boolean unite(K u,K v){
     if ((u = root(u)).equals(v = root(v)))
       return false;
 
+    if (size.get(u) < size.get(v)) {
+      var t = u;
+      u = v;
+      v = t;
+    }
+
     par.put(v,u);
     size.merge(u,size.get(v),Integer::sum);
+    var nu = nxt.get(u);
+    nxt.put(u,nxt.get(v));
+    nxt.put(v,nu);
+
     num--;
     return true;
   }
 
-  public int size(K x){ return size.get(root(x)); }
+  int size(K x){ return size.get(root(x)); }
+
+  public K[] getGroup(K x){
+    x = root(x);
+    K[] ret = Util.cast(Array.newInstance(x.getClass(),size.get(x)));
+    for (int i = 0;i < ret.length;i++)
+      ret[i] = x = nxt.get(x);
+    return ret;
+  }
 }
