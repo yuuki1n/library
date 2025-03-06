96ec2dccdce91eb03ece85f754d5cbf9360a7c75
tusyu <tusyu@192.168.1.19>
2025-03-06 23:11:44 +0900

    BitSet,64分木セット(https://x.com/yuuki_n_n/status/1897629580296618394)

--- src/main/java/library/dataStructure/FastSet.java
+++ src/main/java/library/dataStructure/FastSet.java
@@ -1,26 +1,40 @@
 package library.dataStructure;
 
-public class FastSet{
-  private int K = 5,size;
-  private long[][] data;
+import static java.lang.Math.*;
+import static java.util.Arrays.*;
 
-  public FastSet(){
-    data = new long[K][];
-    for (int i = 0;i < K;i++)
-      data[i] = new long[1 <<(K -i -1) *6];
+public class FastSet{
+  private int size;
+  private long[][] data = new long[1][1];
+
+  public FastSet(){ this(1 <<18); }
+
+  public FastSet(int n){ grow(n); }
+
+  private void grow(int n){
+    int K = (37 -Integer.numberOfLeadingZeros((n = max(data[0].length <<7,n)) -1)) /6;
+    long[][] newData = new long[K][];
+    for (int i = 0;i < data.length;i++)
+      newData[i] = copyOf(data[i],n = n +63 >>6);
+    for (int i = data.length;i < K;i++) {
+      newData[i] = new long[n = n +63 >>6];
+      for (int j = 0;j < newData[i -1].length;j++)
+        if (newData[i -1][j] != 0)
+          newData[i][j >>6] |= 1L <<j;
+    }
+    data = newData;
   }
 
-  public boolean get(int x){ return (data[0][x >>6] >>>(x &63) &1) == 1; }
+  public boolean get(int x){ return x >>6 < data[0].length && (data[0][x >>6] >>>(x &63) &1) == 1; }
 
   public void set(int x){
+    if (data[0].length <= x >>6)
+      grow(x +1);
     if (get(x))
       return;
     size++;
-    for (int i = 0;i < data.length;i++) {
-      int offset = x &63;
-      x >>= 6;
-      data[i][x] |= 1L <<offset;
-    }
+    for (int i = 0;i < data.length;x >>= 6)
+      data[i++][x >>6] |= 1L <<x;
   }
 
   public void clear(int x){
@@ -28,11 +42,8 @@ public class FastSet{
       return;
     size--;
     long lastValue = 0;
-    for (int i = 0;i < data.length && lastValue == 0;i++) {
-      int offset = x &63;
-      x >>= 6;
-      lastValue = data[i][x] &= ~(1L <<offset);
-    }
+    for (int i = 0;i < data.length && lastValue == 0;x >>= 6)
+      lastValue = data[i++][x >>6] &= ~(1L <<x);
   }
 
   public int floor(int x){
@@ -42,22 +53,22 @@ public class FastSet{
       return x;
     for (int i = 0,y = x;i < data.length;i++) {
       int offset = y &63;
-      y = y >>>6;
+      y >>= 6;
       long headMask = offset == 0 ? 0 : -1L >>>64 -offset;
-      if ((data[i][y] &headMask) != 0)
+      if (y < data[i].length && (data[i][y] &headMask) != 0)
         return last(i -1,y <<6 |highestOneBitOffset(data[i][y] &headMask));
     }
     return -1;
   }
 
   public int ceiling(int x){
-    if (1 <<K *6 <= x)
+    if (data[0].length <= x >>6)
       return -1;
     if (get(x))
       return x;
     for (int i = 0,y = x;i < data.length;i++) {
       int offset = y &63;
-      y = y >>>6;
+      y >>= 6;
       long tailMask = offset == 63 ? 0 : -1L <<1 +offset;
       if ((data[i][y] &tailMask) != 0)
         return first(i -1,y <<6 |lowestOneBitOffset(data[i][y] &tailMask));
@@ -82,4 +93,14 @@ public class FastSet{
   private int highestOneBitOffset(long x){ return 63 -Long.numberOfLeadingZeros(x); }
 
   private int lowestOneBitOffset(long x){ return Long.numberOfTrailingZeros(x &-x); }
+
+  public int[] toArray(){
+    if (size == 0)
+      return new int[0];
+    int[] ret = new int[size];
+    ret[0] = first(data.length -1,0);
+    for (int i = 1;i < size;i++)
+      ret[i] = ceiling(ret[i -1] +1);
+    return ret;
+  }
 }
--- src/main/java/library/dataStructure/collection/BitSet.java
+++ src/main/java/library/dataStructure/collection/BitSet.java
@@ -1,53 +1,56 @@
 package library.dataStructure.collection;
 
-import static java.lang.Math.*;
-import static java.util.Arrays.*;
-
-import java.util.*;
-import java.util.ArrayList;
-
 public class BitSet{
   private long[] a;
 
   public BitSet(){ this(1 <<16); }
 
-  public BitSet(int n){ a = new long[n +63 >>6]; }
+  public BitSet(int n){ this(new long[n +63 >>6]); }
 
-  public BitSet or(BitSet b){
-    BitSet ret = new BitSet(max(last(),b.last()) +1);
-    for (int i = 0;i < ret.a.length;i++)
-      ret.a[i] = (i < a.length ? a[i] : 0) |(i < b.a.length ? b.a[i] : 0);
-    return ret;
-  }
+  private BitSet(long[] a){ this.a = a; }
 
-  public BitSet and(BitSet b){
-    BitSet ret = new BitSet(min(last(),b.last()) +1);
-    for (int i = 0;i < ret.a.length;i++)
-      ret.a[i] = a[i] &b.a[i];
-    return ret;
-  }
+  //  public BitSet or(BitSet b){
+  //    BitSet ret = new BitSet(max(last(),b.last()) +1);
+  //    for (int i = 0;i < ret.a.length;i++)
+  //      ret.a[i] = (i < a.length ? a[i] : 0) |(i < b.a.length ? b.a[i] : 0);
+  //    return ret;
+  //  }
+  //
+  //  public BitSet and(BitSet b){
+  //    BitSet ret = new BitSet(min(last(),b.last()) +1);
+  //    for (int i = 0;i < ret.a.length;i++)
+  //      ret.a[i] = a[i] &b.a[i];
+  //    return ret;
+  //  }
+  //
+  //  public BitSet xor(BitSet b){
+  //    BitSet ret = new BitSet(max(last(),b.last()) +1);
+  //    for (int i = 0;i < ret.a.length;i++)
+  //      ret.a[i] = a[i] ^b.a[i];
+  //    return ret;
+  //  }
+  //
+  //  public BitSet shift(int n){
+  //    BitSet ret = new BitSet(last() +n +65);
+  //    int m = abs(n) >>6,r = abs(n) &63;
+  //    if (n < 0) {
+  //      n *= -1;
+  //      for (int i = 0;i < ret.a.length;i++)
+  //        ret.a[i] = a[i +m] >>r |a[i +m +1] <<64 -r >>>64 -r;
+  //    } else
+  //      for (int i = last() >>6;i >= 0;i--) {
+  //        ret.a[i +m] |= a[i] <<r;
+  //        if (r > 0)
+  //          ret.a[i +m +1] |= a[i] >>>64 -r;
+  //      }
+  //    return ret;
+  //  }
 
-  public BitSet xor(BitSet b){
-    BitSet ret = new BitSet(max(last(),b.last()) +1);
-    for (int i = 0;i < ret.a.length;i++)
-      ret.a[i] = a[i] ^b.a[i];
-    return ret;
-  }
-
-  public BitSet shift(int n){
-    BitSet ret = new BitSet(last() +n +65);
-    int m = abs(n) >>6,r = abs(n) &63;
-    if (n < 0) {
-      n *= -1;
-      for (int i = 0;i < ret.a.length;i++)
-        ret.a[i] = a[i +m] >>r |a[i +m +1] <<64 -r >>>64 -r;
-    } else
-      for (int i = last() >>6;i >= 0;i--) {
-        ret.a[i +m] |= a[i] <<r;
-        if (r > 0)
-          ret.a[i +m +1] |= a[i] >>>64 -r;
-      }
-    return ret;
+  public int first(){
+    for (int i = 0;i < a.length;)
+      if (a[i] != 0)
+        return i <<6 |Long.numberOfTrailingZeros(a[i++]);
+    return -1;
   }
 
   public int last(){
@@ -59,21 +62,11 @@ public class BitSet{
 
   public boolean get(int i){ return i >>6 < a.length && (a[i >>6] >>i &1) == 1; }
 
-  public void set(int i){
-    if (a.length <= i >>6)
-      a = copyOf(a,max(a.length <<1,i >>6 +1));
-    a[i >>6] |= 1L <<i;
-  }
+  public void set(int i){ a[i >>6] |= 1L <<i; }
 
-  public void clear(int i){
-    if (i >>6 < a.length)
-      a[i >>6] &= ~(1L <<i);
-  }
+  public void clear(int i){ a[i >>6] &= ~(1L <<i); }
 
-  public void flip(int i){
-    if (i >>6 < a.length)
-      a[i >>6] ^= 1L <<i;
-  }
+  public void flip(int i){ a[i >>6] ^= 1L <<i; }
 
   public int cardinality(){
     int ret = 0;
@@ -83,12 +76,16 @@ public class BitSet{
   }
 
   public int[] toArray(){
-    List<Integer> list = new ArrayList<>();
-    for (int i = 0;i < a.length;i++)
-      if (a[i] != 0)
-        for (int j = 0;j < 64;j++)
-          if ((a[i] >>j &1) == 1)
-            list.add(i *64 +j);
-    return list.stream().mapToInt(a -> a).toArray();
+    int n = cardinality();
+    int[] ret = new int[n];
+    for (int i = 0;i < a.length;i++) {
+      long x = a[i];
+      while (x != 0) {
+        int j = Long.numberOfTrailingZeros(x);
+        ret[ret.length -n--] = i <<6 |j;
+        x ^= 1L <<j;
+      }
+    }
+    return ret;
   }
 }
