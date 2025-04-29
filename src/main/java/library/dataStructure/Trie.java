888e6289261295854451860efdcf1dcaa1db63c5
tusyu <tusyu@yuukiPC>
2025-04-29 10:19:10 +0900

    Trie
    追加時と探索時を別メソッドに
    親情報とそのノードに進んだ時の文字種を持つように
    dfsの機能

--- src/main/java/library/dataStructure/Trie.java
+++ src/main/java/library/dataStructure/Trie.java
@@ -1,6 +1,7 @@
 package library.dataStructure;
 
 import java.lang.reflect.*;
+import java.util.function.*;
 
 import library.dataStructure.collection.*;
 import library.util.*;
@@ -13,27 +14,57 @@ public abstract class Trie<V> {
 
   public Trie(int b){
     this.b = b;
-    root = new Node();
+    root = new Node(null,-1);
   }
 
   abstract V e();
 
-  public MyList<Node> path(char[] s){ return path(Util.arrI(s.length,i -> s[i])); }
+  public MyList<Node> add(char[] s){ return path(Util.arrI(s.length,i -> s[i]),true); }
 
-  public MyList<Node> path(int[] s){
+  public MyList<Node> add(int[] s){ return path(s,true); }
+
+  public MyList<Node> path(char[] s){ return path(Util.arrI(s.length,i -> s[i]),false); }
+
+  public MyList<Node> path(int[] s){ return path(s,false); }
+
+  private MyList<Node> path(int[] s,boolean add){
     MyList<Node> q = new MyList<>(s.length +1);
     var nd = root;
     q.add(nd);
     for (int c:s) {
       if (nd.ch[c] == null)
-        nd.ch[c] = new Node();
+        if (add)
+          nd.ch[c] = new Node(nd,c);
+        else
+          break;
       q.add(nd = nd.ch[c]);
     }
     return q;
   }
 
-  class Node{
-    public Node[] ch = Util.cast(Array.newInstance(this.getClass(),b));
-    public V v = e();
+  public void dfs(Consumer<Node> preFunc,Consumer<Node> sufFunc){ dfs(root,preFunc,sufFunc); }
+
+  public void dfs(Node nd,Consumer<Node> preFunc,Consumer<Node> sufFunc){
+    if (preFunc != null)
+      preFunc.accept(nd);
+    for (var ch:nd.ch)
+      if (ch != null)
+        dfs(ch,preFunc,sufFunc);
+    if (sufFunc != null)
+      sufFunc.accept(nd);
+  }
+
+  public class Node{
+    public Node par;
+    public Node[] ch;
+    public int c;
+    public V v;
+
+    public Node(Node par,int c){
+      this.par = par;
+      ch = Util.cast(Array.newInstance(this.getClass(),b));
+      this.c = c;
+      v = e();
+    }
   }
 }
