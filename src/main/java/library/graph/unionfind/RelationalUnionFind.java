bb46c09dd69e17fd4eeef1f84546915282f37040
tusyu <tusyu@yuukiPC>
2025-05-16 12:39:46 +0900

    RelationalUnionFind リファクタリング

--- src/main/java/library/graph/unionfind/RelationalUnionFind.java
+++ src/main/java/library/graph/unionfind/RelationalUnionFind.java
@@ -3,56 +3,53 @@ package library.graph.unionfind;
 import static java.util.Arrays.*;
 
 public abstract class RelationalUnionFind<F> extends UnionFind{
-  private F[] dist;
+  private F[] rel;
 
   @SuppressWarnings("unchecked")
   public RelationalUnionFind(int n){
     super(n);
-    dist = (F[]) new Object[n];
-    setAll(dist,i -> id());
+    rel = (F[]) new Object[n];
+    setAll(rel,i -> id());
   }
 
   protected abstract F id();
   protected abstract F comp(F a,F b);
   protected abstract F inv(F v);
-  protected abstract boolean eq(F a,F b);
+  protected abstract boolean valid(F a,F b);
+
+  public boolean valid(int u,int v,F c){ return !same(u,v) || valid(rel(u,v),c); }
 
   @Override
   public int root(int x){
     if (dat[x] < 0)
       return x;
-
     int r = root(dat[x]);
-    dist[x] = comp(dist[dat[x]],dist[x]);
-
+    rel[x] = comp(rel[dat[x]],rel[x]);
     return dat[x] = r;
   }
 
-  public boolean valid(int u,int v,F c){ return !same(u,v) || eq(dist(u,v),c); }
-
   @Deprecated
   @Override
-  public boolean unite(int u,int v){ return unite(u,v,id()); }
+  public boolean unite(int u,int v){ throw new UnsupportedOperationException(); }
 
   public boolean unite(int u,int v,F f){
-    if (!valid(u,v,f))
-      return false;
     if (same(u,v))
-      return true;
-    f = comp(dist(u),f);
-    f = comp(f,inv(dist(v)));
+      return valid(rel(u,v),f);
+
+    f = comp(rel(u),f);
+    f = comp(f,inv(rel(v)));
     super.unite(u = root(u),v = root(v));
     if (dat[u] > dat[v])
-      dist[u] = inv(f);
+      rel[u] = inv(f);
     else
-      dist[v] = f;
+      rel[v] = f;
     return true;
   }
 
-  public F dist(int x){
+  public F rel(int x){
     root(x);
-    return dist[x];
+    return rel[x];
   }
 
-  public F dist(int u,int v){ return !same(u,v) ? null : comp(inv(dist(u)),dist(v)); }
+  public F rel(int u,int v){ return !same(u,v) ? null : comp(inv(rel(u)),rel(v)); }
 }
