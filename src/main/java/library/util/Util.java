01753676893a7002c6cedfe7561b198d32272851
tusyu <tusyu@yuukiPC>
2025-07-20 19:15:02 +0900

    Util arr...系

--- src/main/java/library/util/Util.java
+++ src/main/java/library/util/Util.java
@@ -5,6 +5,7 @@ import static java.util.Arrays.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.function.*;
+import java.util.stream.*;
 
 public class Util{
   public static String yes = "Yes",no = "No";
@@ -18,23 +19,11 @@ public class Util{
 
   protected void reset(){ st = System.currentTimeMillis(); }
 
-  public static int[] arrI(int N,IntUnaryOperator f){
-    int[] ret = new int[N];
-    setAll(ret,f);
-    return ret;
-  }
+  public static int[] arrI(int N,IntUnaryOperator f){ return IntStream.range(0,N).map(f).toArray(); }
 
-  public static long[] arrL(int N,IntToLongFunction f){
-    long[] ret = new long[N];
-    setAll(ret,f);
-    return ret;
-  }
+  public static long[] arrL(int N,IntToLongFunction f){ return IntStream.range(0,N).mapToLong(f).toArray(); }
 
-  public static double[] arrD(int N,IntToDoubleFunction f){
-    double[] ret = new double[N];
-    setAll(ret,f);
-    return ret;
-  }
+  public static double[] arrD(int N,IntToDoubleFunction f){ return IntStream.range(0,N).mapToDouble(f).toArray(); }
 
   public static <T> T[] arr(T[] arr,IntFunction<T> f){
     setAll(arr,f);
